d29375a27864d14e69750f8c2bd597b256cde833
Derek Jang <derek.jang@mail.utoronto.ca>
2019-02-17 15:27:30 -0500

    Add SudokuView class
    This commit adds the Sudoku game control buttons consisting of a "New
    Game", "Clear", and "Check" button. These buttons are displayed on the
    left side of the game window.

---
+++ SudokuView.java
@@ -0,0 +1,64 @@
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * This class contains what the player sees on the Sudoku game window.
+ * @author Derek Jang
+ *
+ */
+public class SudokuView implements EventHandler<ActionEvent> {
+	
+	private Stage stage;
+	
+	/**
+	 * Initializes a stage for the Sudoku game window.
+	 * @param stage What is displayed on the screen.
+	 */
+	public SudokuView(Stage stage) {
+		
+		this.stage = stage;
+		initUI(stage);
+	}
+	
+	/**
+	 * Takes a stage and creates the Sudoku game control buttons. 
+	 * @param stage What is displayed on the screen.
+	 */
+	private void initUI(Stage stage) {
+		
+		stage.setTitle("Sudoku290");
+		
+		VBox leftPanel = new VBox();
+		
+		Button newGame = new Button("New Game");
+		newGame.setOnAction(this);
+		
+		Button clear = new Button("Clear");
+		clear.setOnAction(this);
+		
+		Button check = new Button("Check");
+		check.setOnAction(this);
+		
+		leftPanel.getChildren().addAll(newGame, clear, check);
+		
+		BorderPane border = new BorderPane();
+		border.setLeft(leftPanel);
+		
+		Scene scene = new Scene(border);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	/**
+	 * Handles the player's interaction with the control buttons. 
+	 * @param event An ActionEvent with the buttons. 
+	 */
+	public void handle(ActionEvent event) {
+		
+	}
+}
