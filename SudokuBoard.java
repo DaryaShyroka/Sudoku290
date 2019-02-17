0dfc826a272d5b51972abb0e14999f225eacfbc9
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-02-17 12:02:07 -0500

    Add 9x9 grid of Buttons for SudokuBoard
    Regular JavaFx buttons were used instead of CellButttons. Once the
    CellButton class is fully developed, the JavaFx buttons need to be
    replaced with CellButtons.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,10 +1,17 @@
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class SudokuBoard extends GridPane{
+	
 	public SudokuBoard(){
 		
-
-		
+    	for (int i = 0; i < 9; i++) {
+    		for (int k = 0; k < 9; k++) {
+    			Button btn = new Button();
+    			btn.setMinSize(50, 50);
+    			this.add(btn, i, k);   		
+    		}
+    	}		
 	}
 }
