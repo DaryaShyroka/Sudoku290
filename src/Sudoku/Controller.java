04e38defe7efc312a0bc56825f3c754724673b03
Daniel <daniel.ceniceros@mail.utoronto.ca>
2019-03-18 19:29:10 -0400

    Update Controller check helpers' style
    Helper functions for the check method: getRow, getColumn, and getSquare,
    have been moved to SudokuBoard. The nature of these functions were more
    appropriate in SudokuBoard rather than Controller.
    
    Update the helper functions' style; such as replacing variables using
    pot_hole style with the camelCase style, and removing unnecessary
    whitelines that make it inconsistent with the rest of the code.
    
    Fix a typo one of the helper function's docstring.

--- src/Sudoku/Controller.java
+++ src/Sudoku/Controller.java
@@ -89,19 +89,15 @@ public class Controller{
 	/**
 	 * This method will clear every cell which was inputted by the user.
 	 */
-	public void clear() {
-		
-		for(int i = 0; i < 9; i++) {
-			
+	public void clear() {	
+		for(int i = 0; i < 9; i++) {	
 			for(int j = 0; j < 9; j++) {
 				int pos[] = {i,j};
 				updateBoard(pos,0, false); 
 				//[i,j] are the coordinates of the cell, 0 is a placeholder
 				// value for an empty cell.
 			}
-			
-		}
-		
+		}	
 	}
 	
 	/**
@@ -112,66 +108,17 @@ public class Controller{
 	 */
 	
 	public boolean check() {
-		
 		for(int i = 0; i < 9; i++) { // check if the rows are complete
-			
-			int[] rows = this.getRow(i);
-			int[] cols = this.getCol(i);
-			int[] x9sq = this.getSquare(i);
-			
+			int[] rows = currentBoard.getRow(i);
+			int[] cols = currentBoard.getCol(i);
+			int[] x9sq = currentBoard.getSquare(i);
 			if(!checkUnique(rows) || !checkUnique(cols) || !checkUnique(x9sq)) {
 				return false;
 			}
-			
 		}
-		
 		return true;
-		
 	}
 	
-	/**
-	 * Returns a row of the current sudoku board.
-	 * @param indice An integer from 0 to 8.
-	 * @return The row of the sudoku board going from top to bottom.
-	 */
-	private int[] getRow(int indice){
-		return (currentBoard.getBoard())[indice];
-	}
-	
-	/**
-	 * Returns a column of the current sudoku board.
-	 * @param indice An integer from 0 to 8.
-	 * @return The colum of the sudoku board going from left to right.
-	 */
-	private int[] getCol(int indice){
-		int[] board_column = new int[9];
-		int[][] currBoard = currentBoard.getBoard();
-		for(int x = 0; x < 9; x++) {
-			board_column[x] = currBoard[x][indice];
-		}
-		return board_column;
-	}
-	
-	/**
-	 * Returns an inner 3x3 square of the current sudoku board.
-	 * @param indice An integer from 0 to 8.
-	 * @return The corresponding inner 3x3 square of the sudoku board, going from
-	 * top-left to bottom-right, iterating to the right before iterating down,
-	 * represented as an array of 9 integers.
-	 */
-	private int[] getSquare(int indice){
-		int[] board_square = new int[9];
-		int[][] currBoard = currentBoard.getBoard();
-		int x_start = (indice % 3)*3;
-		int y_start = Math.floorDiv(indice, 3)*3;
-		for(int x = 0; x < 3; x++) {
-			for(int y = 0; y < 3; y++) {
-				board_square[(Math.floorDiv(x, 3)*3) + y] = 
-						currBoard[x_start + x][y_start + y];
-			}
-		}
-		return board_square;
-	} 
 	
 	/**
 	 * Precondition: ilist has exactly 9 integers.
@@ -180,36 +127,28 @@ public class Controller{
 	 * @return
 	 */
 	private boolean checkUnique(int[] ilist){
-		
 		int keepTrack = 1;
 		boolean foundIt = false;
-		
-		while(keepTrack <= 9) {
-			
-			for(int i = 0; i < 9; i++) {
-				
+		while(keepTrack <= 9) {	
+			for(int i = 0; i < 9; i++) {	
 				if(ilist[i] == keepTrack || !foundIt) {
 					foundIt = true;
 				}else if(ilist[i] == keepTrack || foundIt) {
 					return false; //there is a duplicate value.
 				}
-				
 			}
-			
 			if(!foundIt) {
 				return false; //we have not found a required number
-			}else {
+			}else{
 				//we have found the required number exactly one, and so
 				//we will look for the next number and do the same process.
 				foundIt = false;
 				keepTrack++;
 			}
-			
 		}
 		//if it reaches outside of the for-loop, it has found exactly one
 		//of every integer from 1 to 9.
 		return true;
-		
 	}
 	
 	/**
--- src/Sudoku/SudokuBoard.java
+++ src/Sudoku/SudokuBoard.java
@@ -64,10 +64,51 @@ public class SudokuBoard {
 				this.board[x][y] = value;
 				view.update(value, x, y);
 			}
-		}
-		
-		
+		}	
 	}
+	
+	
+	/**
+	 * Returns a row of the current sudoku board.
+	 * @param indice An integer from 0 to 8.
+	 * @return The row of the sudoku board going from top to bottom.
+	 */
+	public int[] getRow(int indice){
+		return this.board[indice];
+	}
+	
+	/**
+	 * Returns a column of the current sudoku board.
+	 * @param indice An integer from 0 to 8.
+	 * @return The column of the sudoku board going from left to right.
+	 */
+	public int[] getCol(int indice){
+		int[] boardColumn = new int[9];
+		for(int x = 0; x < 9; x++) {
+			boardColumn[x] = this.board[x][indice];
+		}
+		return boardColumn;
+	}
+	
+	/**
+	 * Returns an inner 3x3 square of the current sudoku board.
+	 * @param indice An integer from 0 to 8.
+	 * @return The corresponding inner 3x3 square of the sudoku board, going from
+	 * top-left to bottom-right, iterating to the right before iterating down,
+	 * represented as an array of 9 integers.
+	 */
+	public int[] getSquare(int indice){
+		int[] boardSquare = new int[9];
+		int xStart = (indice % 3)*3; //capture the leftmost column of the indice's inner square
+		int yStart = Math.floorDiv(indice, 3)*3; //capture the top row of the indice's inner square
+		for(int x = 0; x < 3; x++) {
+			for(int y = 0; y < 3; y++) {
+				boardSquare[(Math.floorDiv(x, 3)*3) + y] = 
+						this.board[xStart + x][yStart + y];
+			}
+		}
+		return boardSquare;
+	} 
 
 	/**
 	 * Starts the game.
